5869d620704c7bbcee8373ab311c553769b63bca
v1rtl <hi@v1rtl.site>
2023-09-18 16:43:40 +0300

    move manifest to a function

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       {/* PWA config */}
-      <link rel="manifest" href="/manifest.json" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
       <meta name="apple-mobile-web-app-title" content="Fleek PWA" />
